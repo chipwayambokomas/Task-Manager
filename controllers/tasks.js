a987b93d9b0ce280ff431e7d640d5bec715a8081
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-13 19:10:01 +0200

    Set up syntax for the rest of thr controllers and routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req,res) => {
-    res.send('all items')
+    res.send('Get all task')
+}
+
+const createTask = (req,res) => {
+    res.send('Create task')
+}
+
+const getTask = (req,res) => {
+    res.send('Get single task')
+}
+
+const editTask = (req,res) => {
+    res.send('Edit task')
+}
+
+const deleteTask = (req,res) => {
+    res.send('Delete task')
 }
 
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    editTask,
+    deleteTask
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {getAllTasks} = require('../controllers/tasks')
+const {getAllTasks, createTask, getTask, editTask,deleteTask} = require('../controllers/tasks')
 
-router.route('/').get(getAllTasks)
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(editTask).delete(deleteTask)
 
 module.exports = router
