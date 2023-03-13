535460283773d3ac92397eda682fe08eee4de730
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-13 21:40:02 +0200

    Secured database password using dotenv

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
-require('./db/connect')
+const connectDB = require('./db/connect')
+require("dotenv").config()
 //MIDDLEWARE
 
 // This allows us to get the request body
@@ -12,6 +13,17 @@ app.use('/api/v1/tasks' , tasks)
 
 
 const PORT = 3000
-app.listen(PORT, () => {
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}...`);
 })
+    } catch (error) {
+        console.log(err0r);
+    }
+}
+
+start()
+
